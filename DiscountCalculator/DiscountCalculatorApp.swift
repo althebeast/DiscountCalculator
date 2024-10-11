18592f79c0e3823e0ec1907c29f1826af5008ab8
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-10-10 22:12:04 -0400

    Initial Commit

---
+++ DiscountCalculator/DiscountCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountCalculatorApp.swift
+//  DiscountCalculator
+//
+//  Created by Alperen Sarışan on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscountCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
